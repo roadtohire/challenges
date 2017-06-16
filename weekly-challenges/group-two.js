475f444b983ce80d2baf839e5e8fab9a464f46e1
Tabitha Chandler <tabithalessthanthree@gmail.com>
2017-06-16 14:19:23 -0400

    Delete group-two.js

--- weekly-challenges/group-two.js
+++
@@ -1,9 +0,0 @@
-// EASY: Write a function that takes in two separate parameters and console log a sentence with both parameters concatenated together into one string.
-// EASY: Write a function that accepts three separate colors and returns an array.  Console log the result of the function.
-// EASY: Use the function created in the array challenge and loop through the array to console log each color individually.
-// MEDIUM: Using the function you created to make an array from the previous challenge, make another array of objects that have an animal and a corresponding color in each object. Make sure the first letter of the animal and the first letter of the color are the same.
-// Console log a sentence for each animal and its color. EX. “The giraffe is green”.
-// HARD:
-// In this challenge, you will use the same functionality from the medium challenge. You will need a total of 6 animals and 6 colors. Instead of having the animals and colors matching by first letter, they will need to be completely random.
-// Create a string, similar to the medium challenge, and measure its length. Compare the length of all the strings to find out which has the most number of characters. Console log this sentence out with the number of characters it contains.
-// EX. "The string with the most characters is 'The RattleSnake is yellow' with 25”.
